5fcc204179ff79fdabde73516acf1f1c105f38e8
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2020-01-13 23:24:28 +0530

    add github API

--- utils.js
+++ utils.js
@@ -5,10 +5,17 @@ async function getProjects(groupId,token) {
         const response= await axios.get(`https://gitlab.com/api/v4/groups/${groupId}/projects?access_token=${token}`)
         return response.data
     }catch(err) {
-        console.log(err)
+        console.error(err.errno)
     }
 }
-
+async function getRepos(groupName,token) {
+    try {
+        const response=await axios.get(`https://api.github.com/orgs/${groupName}/repos`,{ headers: { Authorization:`token ${token}`}})
+        return response.data
+    }catch(err) {
+        console.error(err)
+    }
+}
 async function getHooks(projectId,token) {
     try{
         const response= await axios.get(`https://gitlab.com/api/v4/projects/${projectId}/hooks?access_token=${token}`)
@@ -17,7 +24,14 @@ async function getHooks(projectId,token) {
         console.log(err)
     }
 }
-
+async function githubHooks(groupName,repoName,token) {
+    try{
+        const response=await axios.get(`https://api.github.com/repos/${groupName}/${repoName}/hooks`,{ headers: { Authorization:`token ${token}`}})
+        return response.data
+    }catch(err) {
+        console.error(err)
+    }
+}
 async function getGroups(token) {
     try{
         const response= await axios.get(`https://gitlab.com/api/v4/groups?access_token=${token}`)
@@ -42,7 +56,27 @@ async function editHook(projectId,hook,searchURL,replaceURL,token) {
         })
 
     }catch(err) {
-        console.log(err)
+        console.error(err)
+    }
+}
+async function editGHook(orgName,repoName,hook,searchURL,replaceURL,token) {
+    try {
+        let hookId=hook.id
+        let hookURL=hook.url
+        if(!hookURL.includes(searchURL))
+        {
+            return
+        }
+        let newURL=hookURL.replace(/(?:^https?:\/\/([^\/]+)(?:[\/,]|$)|^(.*)$)/,replaceURL)
+        console.log(hookId)
+        console.log(newURL)
+        const response=await  axios.patch(`https://api.github.com/repos/${orgName}/${repoName}/hooks/${hookId}`,{ headers: { Authorization:`token ${token}`}},{
+            config:{
+                url:newURL
+            }
+        })
+    }catch(err) {
+        console.error(err)
     }
 }
 function filterFunction(array,regEx) {
@@ -84,5 +118,8 @@ module.exports={
     getProjects:getProjects,
     getHooks:getHooks,
     editHook:editHook,
-    filterFunction:filterFunction
+    filterFunction:filterFunction,
+    getRepos:getRepos,
+    githubHooks:githubHooks,
+    editGHook:editGHook
 }
