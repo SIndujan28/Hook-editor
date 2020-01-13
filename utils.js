69fde9c64d43fd818fee901570786da49c98b1e0
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2020-01-13 12:16:57 +0530

    add get project fn

---
+++ utils.js
@@ -0,0 +1,11 @@
+const axios=require('axios')
+
+async function getProjects(groupId,token) {
+    try{
+        const response= await axios.get(`https://gitlab.com/api/v4/groups/${groupId}/projects?access_token=${token}`)
+        return response.data
+    }catch(err) {
+        console.log(err)
+    }
+}
+
